8a2eb3d9a6b7a971ad9d261f989b2aaa5bd8bcaf
Renan Feliciano <renan.feliciano.rocha@gmail.com>
2022-03-25 14:18:46 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/alura/bytebank_ofc/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.alura.bytebank_ofc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ByteBankApp());
+}
+
+class ByteBankApp extends StatelessWidget {
+  const ByteBankApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('DashBoard'),
+        ),
+      ),
+    );
+  }
+}
+
