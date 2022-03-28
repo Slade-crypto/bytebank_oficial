e6c056e517d81c8fa95c9d70d2839b185fa0a1bc
Renan Feliciano <renan.feliciano.rocha@gmail.com>
2022-03-28 16:33:13 -0300

    concluido a criação do banco
    Adicionar dependências no projeto;
    Configurar o sqflite;
    Criar banco de dados e tabela;
    Salvar e buscar dados do banco.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:bytebank_ofc/model/contact.dart';
 import 'package:bytebank_ofc/screens/contact_form.dart';
 import 'package:bytebank_ofc/screens/contacts_list.dart';
 import 'package:bytebank_ofc/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
+import 'database/app_database.dart';
+
 void main() {
   runApp(const ByteBankApp());
+  save(Contact(1, 'Alex', 1000)).then((id){
+    findAll().then((contacts) => debugPrint(contacts.toString()));
+  });
 }
 
 class ByteBankApp extends StatelessWidget {
