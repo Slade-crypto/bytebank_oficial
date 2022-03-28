b7556b4b29aa1e2f244422242e6082a450ae8ff9
Renan Feliciano <renan.feliciano.rocha@gmail.com>
2022-03-28 15:29:24 -0300

    Screen creation with a form

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:bytebank_ofc/screens/contact_form.dart';
+import 'package:bytebank_ofc/screens/contacts_list.dart';
+import 'package:bytebank_ofc/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,52 +28,3 @@ class ByteBankApp extends StatelessWidget {
   }
 }
 
-class DashBoard extends StatelessWidget {
-  const DashBoard({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DashBoard'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/bytebank_logo.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              height: 100,
-              width: 150,
-              color: Theme.of(context).primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(
-                    Icons.people,
-                    color: Colors.white,
-                    size: 24.0,
-                  ),
-                  Text(
-                    'Contacts',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,44 @@
+import 'package:bytebank_ofc/screens/contact_form.dart';
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatelessWidget {
+  const ContactsList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text('Contacts'),
+      ),
+      body: ListView(
+        children: const [
+          Card(
+            child: ListTile(
+              title: Text(
+                'Alex',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              subtitle: Text(
+                '1000',
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ContactForm(),
+            ),
+          ).then((newContact) => debugPrint(newContact.toString()));
+        },
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
